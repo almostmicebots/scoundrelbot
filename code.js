1fabddeebde065a5f546e68bc0223ddcdb6d3c49
Isaac <ifb9649@rit.edu>
2020-08-09 21:28:20 +0000

    Got help from a friend who knows js to look at code

--- code.js
+++ code.js
@@ -29,7 +29,35 @@ bot.on('message', message => {
         response = "pong";
     }
 
-    message.channel.send(response);
+    if(response != '' && response != null)
+        message.channel.send(response);
 });
 
 bot.login(token);
+
+
+bot.on('ready',()=>{
+
+    //bot.setTimeout(rat_time, millisecondsTilFour);
+    var channel = bot.channels.cache.get('695758384711860244');
+
+    setTimeout(() => {
+        //console.log(channel.type);
+        channel.send("it is rat time")
+           .catch(console.error);
+      }, ( new Date().setHours(20, 12, 0, 0) - Date.now() + 1000*60*60*24 ) % (1000*60*60*24) );
+
+
+    bot.user.setActivity('Bad Rats', { type: 'PLAYING' })
+        .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+        .catch(console.error);
+});
+
+function rat_time(){
+    var channel = bot.channels.cache.get('695758384711860244');
+    console.log(channel.type);
+    channel.send("it is rat time")
+       .catch(console.error);
+}
+
+
